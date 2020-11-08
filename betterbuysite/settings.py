3da3fd8ad0f5c0f798f1a8e7b0223a3ded966926
rynewhitaker <rynewhitaker@pm.me>
2020-11-08 17:18:33 -0600

    Changed VAT to 9 percent

--- betterbuysite/settings.py
+++ betterbuysite/settings.py
@@ -598,7 +598,7 @@ ELASTICSEARCH_DSL = {
 ############################################
 # settings for django-shop and its plugins
 
-SHOP_VALUE_ADDED_TAX = Decimal(19)
+SHOP_VALUE_ADDED_TAX = Decimal(9)
 SHOP_DEFAULT_CURRENCY = 'USD'
 SHOP_EDITCART_NG_MODEL_OPTIONS = "{updateOn: 'default blur', debounce: {'default': 2500, 'blur': 0}}"
 
