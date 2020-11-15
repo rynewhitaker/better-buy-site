4f2e7f65c96135749efefa44eea52ebf9c5bbb48
Jafett Puga <pjafett1999@gmail.com>
2020-11-15 17:00:56 -0600

    Connected template to view.

--- DroneDelivery/views.py
+++ DroneDelivery/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
 def index(request):
-    return HttpResponse("DroneDelivery")
+    context = {}
+    template = loader.get_template("DroneDelivery/index.html")
+    return render(request, 'DroneDelivery/index.html')
