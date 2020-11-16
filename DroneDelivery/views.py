fa741d0484eb50433fa6574e4b9c44902dda0641
Jafett Puga <pjafett1999@gmail.com>
2020-11-15 18:12:57 -0600

    Connected base template to Drone Shipping view

--- DroneDelivery/views.py
+++ DroneDelivery/views.py
@@ -2,8 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
+from shop.models.defaults.order import Order
+from betterbuysite.models import Product
+
 # Create your views here.
 def index(request):
-    context = {}
     template = loader.get_template("DroneDelivery/index.html")
-    return render(request, 'DroneDelivery/index.html')
+    queryset = {
+        'drone_orders': Order.objects.all(),
+        'items' : Product.objects.all(),
+    }
+    return render(request, 'DroneDelivery/index.html', queryset)
