718884ee9e3eca864dab00bb1b004551af771e82
Ryne Whitaker <rynewhitaker@pm.me>
2020-11-17 17:36:45 -0600

    12velos12 Updated copier with new functionality

--- betterbuysite/copier.py
+++ betterbuysite/copier.py
@@ -27,14 +27,38 @@ site_cursor = site_orders.cursor()
 
 site_rows = site_cursor.execute("SELECT * FROM betterbuysite_order WHERE extra LIKE '%drone-delivery%'").fetchall()
 drone_rows = drone_cursor.execute("SELECT * FROM drone_order").fetchall()
+drone_assignment_rows = drone_cursor.execute("SELECT * FROM drone_assignment").fetchall()
 
-def isExistingRow(site_row):
-    for i in range(0,len(drone_rows)):
-        if(site_row[0] == drone_rows[i][0]):
-            return True
-    return False
+def addDroneOrderRow(newDroneRow):
+    drone_cursor.execute("INSERT INTO drone_order VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)", newDroneRow)
 
-def addDeliveryStatus(site_row):
+def changeDroneDeliveryStatus(drone_order_id, newStatus): #newStatus should only be 'waiting', 'busy', and 'delivered'
+    drone_cursor.execute("UPDATE drone_order SET delivery_status = (?) WHERE id = (?)", (newStatus, drone_order_id))
+
+def assignDrone(drone_id, order_id):
+    drone_cursor.execute("UPDATE drone_assignment SET order_id = (?) WHERE drone_id = (?)", (order_id, drone_id))
+
+def unassignDrone(drone_id):
+    drone_cursor.execute("UPDATE drone_assignment SET order_id = NULL WHERE drone_id = (?)", [drone_id])
+
+def getAssignedDroneRows():
+    assignedDrones = drone_cursor.execute("SELECT * FROM drone_assignment WHERE order_id IS NOT NULL").fetchall()
+    return assignedDrones
+
+def getUnassignedDroneRows():
+    unassignedDrones = drone_cursor.execute("SELECT * FROM drone_assignment WHERE order_id IS NULL").fetchall()
+    return unassignedDrones
+
+def getAssignedDroneOrder(drone_id):
+    order_id = drone_cursor.execute("SELECT order_id FROM drone_assignment WHERE drone_id is (?)", [drone_id]).fetchall()
+    return order_id[0][0]
+
+def droneDeliveryComplete(drone_id):
+    order_id = getAssignedDroneOrder(drone_id)
+    unassignDrone(drone_id)
+    changeDroneDeliveryStatus(order_id, "delivered")
+
+def site_row_TO_drone_row(site_row):
     drone_order_row = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
     for i in range(0,len(site_row)):
         drone_order_row[i] = site_row[i]
@@ -42,26 +66,36 @@ def addDeliveryStatus(site_row):
     return drone_order_row
 
 def updateDroneDb():
+
+    def isExistingRow(site_row):
+        for i in range(0,len(drone_rows)):
+            if(site_row[0] == drone_rows[i][0]):
+                return True
+        return False
+
     for i in range(0,len(site_rows)):
         if(isExistingRow(site_rows[i])):
             continue
-        drone_cursor.execute("INSERT INTO drone_order VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)", addDeliveryStatus(site_rows[i]))
-
-def droneExists(droneID, drone_assignment_rows):
-    for i in range(0, len(drone_assignment_rows)):
-        if(droneID == drone_assignment_rows[i][0]):
-            return True
-    return False
+        addDroneOrderRow(site_row_TO_drone_row(site_rows[i]))
 
 def instantiateDrones(droneAmount):
-    drone_assignment_rows = drone_cursor.execute("SELECT * FROM drone_assignment").fetchall()
+
+    def droneExists(droneID):
+        for i in range(0, len(drone_assignment_rows)):
+            if(droneID == drone_assignment_rows[i][0]):
+                return True
+        return False
+
     for i in range(1,droneAmount + 1):
-        if(droneExists(i,drone_assignment_rows)):
+        if(droneExists(i)):
             continue
         drone_cursor.execute("INSERT INTO drone_assignment (drone_id) VALUES (?)", [i])
 
-def assignOrders():
-    availableDronesRows = drone_cursor.execute("SELECT * FROM drone_assignment WHERE order_id IS NULL").fetchall()
+def assignDroneOrders():
+    droneAmount = 20
+    instantiateDrones(droneAmount)
+
+    availableDronesRows = getUnassignedDroneRows()
     availableOrdersRows = drone_cursor.execute("SELECT * FROM drone_order WHERE delivery_status IS 'waiting'").fetchall()
     availableDronesNum = len(availableDronesRows)
     availableOrdersNum = len(availableOrdersRows)
@@ -69,18 +103,13 @@ def assignOrders():
     for i in range(0, availableDronesNum):
         if(availableOrdersNum <= i):
             break
-        drone_cursor.execute("UPDATE drone_assignment SET order_id = (?) WHERE drone_id = (?)", (availableOrdersRows[i][0], availableDronesRows[i][0]))
-        drone_cursor.execute("UPDATE drone_order SET delivery_status = (?) WHERE id = (?)", ('busy', availableOrdersRows[i][0]))
-
-def assignDroneOrders():
-    droneAmount = 10
-    instantiateDrones(droneAmount)
-    assignOrders()
+        assignDrone(availableDronesRows[i][0], availableOrdersRows[i][0])
+        changeDroneDeliveryStatus(availableOrdersRows[i][0], "busy")
 
 def resetDroneAssignmentTable():
     drone_cursor.execute("DELETE FROM 'drone_assignment'")
     for i in range(0, len(drone_rows)):
-        drone_cursor.execute("UPDATE drone_order SET delivery_status = (?) WHERE id = (?)", ('waiting', drone_rows[i][0]))
+        changeDroneDeliveryStatus(drone_rows[i][0], "waiting")
 
 updateDroneDb()
 assignDroneOrders()
