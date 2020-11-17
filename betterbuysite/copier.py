075a07e58796436f0d006700ab99e05f38d733bc
Ryne Whitaker <rynewhitaker@pm.me>
2020-11-17 16:12:58 -0600

    12velos12 fixed bug in copier

--- betterbuysite/copier.py
+++ betterbuysite/copier.py
@@ -38,7 +38,7 @@ def addDeliveryStatus(site_row):
     drone_order_row = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
     for i in range(0,len(site_row)):
         drone_order_row[i] = site_row[i]
-    drone_order_row[14] = 'false'
+    drone_order_row[14] = 'waiting'
     return drone_order_row
 
 def updateDroneDb():
